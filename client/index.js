334880a7cbc6977d38823f724a66501c4954e9d9
Jess Freasier <jfreasier@gmail.com>
2017-09-11 12:01:46 -0500

    added basic react/redux elements

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <div>Hello, FullstackTRIO! You rock!! WOO!!</div>,
+  <Provider store={store}>
+    <div>Hello, FullstackTRIO! You rock!! WOO!!</div>
+  </Provider>,
   document.getElementById('main') // make sure this is the same as the id of the div in your index.html
 );
