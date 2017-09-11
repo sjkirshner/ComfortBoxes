4b9ce038aee12bcc261e15b6032c0342cc5eed45
Sara Jo Helba <sarahelba@saras-mbp.lan>
2017-09-11 12:42:15 -0400

    added start scripts among other things

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Hello, FullstackTRIO! You rock!! WOO!!</div>,
+  document.getElementById('main') // make sure this is the same as the id of the div in your index.html
+);
