214b6f153d6ae34ae07a3c99fd2555c559d87c08
Jess Freasier <jfreasier@gmail.com>
2017-09-11 15:18:28 -0500

    secrets & sequelize session store

--- server/_db.js
+++ server/_db.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 
-
-
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/boilerplate', {
   logging: false // unless you like the logs
   // ...and there are many other options you may want to play with
 });
+
+module.exports = db;
+
 require('./models')
 
 
-module.exports = db;
--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const path = require('path')
 const bodyParser = require('body-parser');
 const db = require('./_db.js');
 
-
 const morgan = require('morgan');
 app.use(morgan('dev'));
 
@@ -14,6 +13,25 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+require('../secrets')
+const session = require('express-session');
+
+// configure and create our database store
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const dbStore = new SequelizeStore({ db: db });
+
+// sync so that our session table gets created
+dbStore.sync();
+
+// plug the store into our session middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  store: dbStore,
+  resave: false,
+  saveUninitialized: false
+}));
+
 
 app.use('/api', require('./apiRoutes')); // matches all requests to /api
 
@@ -29,7 +47,7 @@ app.use(function (err, req, res, next) {
 });
 
 
-db.sync()  // sync our database
+db.sync({force: true})  // sync our database
   .then(function(){
     console.log('Everything but the kitchen sync!')
     const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
