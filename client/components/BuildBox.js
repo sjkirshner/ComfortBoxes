e1e5bfeb7b02bd9b83fb9548e9b613c2f042afa0
SangWoo Kim <sangwoo1111@gmail.com>
2017-09-14 13:46:40 -0700

    organized styles & have basic styling on buildbox page

--- client/components/BuildBox.js
+++ client/components/BuildBox.js
@@ -1,12 +1,43 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-
+//BUILD BOX CONTAINER
+//displays all categories and products for a selected category
 export default class BuildBox extends Component {
+  componentDIdMount () {
+
+  }
+
   render () {
     return (
-      <div className='page'>
-        Build My Box Page
+      <div id='buildboxPage' className='page'>
+        <div className='categoryList'>
+          Categories
+        </div>
+        <div className='productList'>
+          {
+            [1,2,3,4,5,6,7,8].map(i => {
+              return (
+                <div key={i} className='product'>
+                  <img src='https://blog.mrprintables.com/wp-content/uploads/2013/10/1_Instant_Comfort_pocket_box_craft_little_gatherer.jpg' alt='product_image'/>
+                  <div>Product Name</div>
+                  <button>Add</button>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     );
   }
 }
+
+// function mapStateToProps(state){
+
+// }
+
+// function mapDispatchToProps(dispatch){
+
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(BuildBox);
