cd018fe3f1b142da40f2ffb81712a14325142559
SangWoo Kim <sangwoo1111@gmail.com>
2017-09-14 08:38:03 -0700

    displaying homepage with navbar

---
+++ client/components/BuildBox.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+export default class BuildBox extends Component {
+  render () {
+    return (
+      <div className='page'>
+        Build My Box Page
+      </div>
+    );
+  }
+}
---
+++ client/components/SignUp.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+export default class SignUp extends Component {
+  render () {
+    return (
+      <div className='page'>
+        SignUp Page
+      </div>
+    );
+  }
+}
