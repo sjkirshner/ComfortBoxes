1adf6bb776578b3b8f042981853a96d5259fdce6
Sara Jo Helba <sarahelba@saras-mbp.lan>
2017-09-14 19:09:33 -0400

    completed Category default scope including products and get all categories express route

---
+++ server/api/categories.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const {Category} = require('../db/models')
+module.exports = router
+
+// GET /api/categories/
+// res.json array of category objects (that have arrays of product objects attached to each)
+router.get('/', (req, res, next) => {
+  Category.findAll()
+    .then(categories => {
+      res.json(categories)
+    })
+    .catch((err) => console.error(err));
+})
+
