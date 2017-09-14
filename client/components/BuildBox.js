97c19996554a1a0be13a94e5c33a0035ef2e92ba
SangWoo Kim <sangwoo1111@gmail.com>
2017-09-14 15:01:38 -0700

    redux in comments

--- client/components/BuildBox.js
+++ client/components/BuildBox.js
@@ -4,15 +4,48 @@ import { connect } from 'react-redux';
 //BUILD BOX CONTAINER
 //displays all categories and products for a selected category
 export default class BuildBox extends Component {
-  componentDIdMount () {
-
+  componentDidMount () {
+    // fetch categories and products
   }
 
+  /**
+   * Category.{fieldName}
+   * Category.product === ARRAY w/ each idx a product
+   * Fetch all categories
+   *   GET '/api/categories'
+   *      Category.findAll()
+   *        .then(categories => {
+   *          res.json(categories)
+   *        })
+   *        .catch(console.error.bind(error));
+   *
+   * function thunkFetchCategories() {
+   *   return (dispatch, getState) => {
+   *     axios.get('/api/categories')
+   *      .then(res => res.data)
+   *      .then(categories => {
+   *        dispatch( someActionCreator(categories) )
+   *      })
+   *   }
+   * }
+   *
+   *
+   *
+   */
   render () {
     return (
       <div id='buildboxPage' className='page'>
         <div className='categoryList'>
-          Categories
+          {
+            ['Box', 'Sights', 'Tastes', 'Smells', 'Touch', 'Sound'].map((title, i) => {
+              return (
+                <div key={i} className='category'>
+                  <img src='https://blog.mrprintables.com/wp-content/uploads/2013/10/1_Instant_Comfort_pocket_box_craft_little_gatherer.jpg' alt='category_image'/>
+                  <div>{title}</div>
+                </div>
+              )
+            })
+          }
         </div>
         <div className='productList'>
           {
@@ -33,11 +66,11 @@ export default class BuildBox extends Component {
 }
 
 // function mapStateToProps(state){
-
+// get states from store
 // }
 
 // function mapDispatchToProps(dispatch){
-
+// get thunk or actioncreators from store
 // }
 
 // export default connect(mapStateToProps, mapDispatchToProps)(BuildBox);
