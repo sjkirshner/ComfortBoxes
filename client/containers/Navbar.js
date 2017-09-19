b4064229fefc65f6e7f6158941eda40ccc14641c
Jess Freasier <jfreasier@gmail.com>
2017-09-19 17:04:36 -0500

    fixed the redirect issue

--- client/containers/Navbar.js
+++ client/containers/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { logout, me } from '../store/user';
 import { createBoxInShoppingCart } from '../shoppingCart'
 
@@ -37,14 +37,17 @@ export class Navbar extends Component {
   render () {
     console.log('currentUser', this.props.currentUser)
     const {currentUser, handleLogout} = this.props;
+    // console.log(typeof this.props.location.pathname)
 
     return (
       <div className='navbar'>
-        <Link to='/' className='navHome' onClick={this.showBuildBox}>Home</Link>
+        <Link to='/' className='navHome'>Home</Link>
         <div className='nav'>
-          <Link to='/buildbox/Box' style={{visibility: this.state.buildBoxHidden}} onClick={this.createABox}>Build My Box</Link>
-          <Link to='/cart' onClick={this.showBuildBox}>Cart</Link>
-          <Link to='/checkout' onClick={this.showBuildBox}>Checkout</Link>
+          {!this.props.location.pathname.includes('buildbox') &&
+            <Link to='/buildbox/Box' onClick={this.createABox}>Build My Box</Link>
+          }
+          <Link to='/cart'>Cart</Link>
+          <Link to='/checkout'>Checkout</Link>
           {
             currentUser.email
               ? <a href='' onClick={handleLogout}>Sign Out</a>
@@ -59,4 +62,4 @@ export class Navbar extends Component {
 const mapState = ({currentUser}) => ({currentUser});
 const mapDispatch = { handleLogout: logout, me }
 
-export default connect(mapState, mapDispatch)(Navbar);
+export default withRouter(connect(mapState, mapDispatch)(Navbar));
