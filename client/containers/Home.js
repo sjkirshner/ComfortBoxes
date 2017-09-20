e9a02e0474cb358439c4517bfcbb77cd20b99364
Sara Jo Helba <sarahelba@saras-mbp.lan>
2017-09-20 17:10:51 -0400

    added description to home page

--- client/containers/Home.js
+++ client/containers/Home.js
@@ -16,6 +16,8 @@ export class Home extends Component {
         <div id='welcome'>
           <h1><b>Welcome</b></h1>
           <h3><b>{this.props.currentUser.email || ''}</b></h3>
+          <h4>Find your comfort with a <b>comfort box</b></h4>
+          <h5 id='description' >Our comfort boxes are designed to ground you during times of stress<br/>by engaging all of your senses with the items <b>you</b> find most comforting</h5>
       </div>
       </div>
     )
